379b16b17cfa277c3f80ce74a80c14f3232bf269
Bosses <braulio.osses.m@gmail.com>
2016-06-14 16:21:16 -0400

    avances
    falta crear la funcion que genere equipos

--- team-maker/controllers/MainController.js
+++ team-maker/controllers/MainController.js
@@ -1,18 +1,24 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Top Sellers in Books lalal'; 
   $scope.promo ='hola';
-  $scope.products = [ 
-  { 
-    name: 'The Book of Trees', 
-    price: 19, 
-    pubdate: new Date('2014', '03', '08'), 
-    cover: 'img/the-book-of-trees.jpg' 
-  }, 
-  { 
-    name: 'Program or be Programmed', 
-    price: 8, 
-    pubdate: new Date('2013', '08', '01'), 
-    cover: 'img/program-or-be-programmed.jpg' 
-  } 
-];
+  $scope.pila = [];
+  $scope.update = function(user) {
+  		if($scope.jugador 
+  			&& $scope.jugador.nombre!='' 
+  			&& $scope.jugador.valor!=''
+  			&& $scope.jugador.nombre!=undefined
+  			&& $scope.jugador.valor!=undefined
+  			&& $scope.pila.length<=12){
+  			$scope.pila.push(user);
+       		$scope.clear();
+  		}else{
+  			console.log('inserte valores o ya tiene muchos jugadores')
+  		}
+      };
+  $scope.clear = function(){
+  	$scope.jugador=null;
+  };
+  $scope.generar = function(){
+  	
+  }
 }]);
